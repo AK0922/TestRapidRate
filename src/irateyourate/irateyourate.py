660770545bbaa1ab4b19abb41961adcc77f69c25
Vineet John <vineet.john@uwaterloo.ca>
2016-11-12 18:08:34 -0500

    Cleaned up the Options class

--- src/irateyourate/irateyourate.py
+++ src/irateyourate/irateyourate.py
@@ -1,13 +1,35 @@
 import sys
 
 from src.irateyourate.processors.amazon_review_processor import AmazonReviewProcessor
-from src.irateyourate.utils import options
+from src.irateyourate.utils.options import Options
+from argparse import ArgumentParser
 
 
 def main(argv):
-    options.parse_args(argv)
+    """
+    Main function to kick start execution
+    :param argv:
+    :return: null
+    """
+    parse_args(argv)
     processor = AmazonReviewProcessor()
     processor.process()
+
+
+def parse_args(argv):
+    """
+    Parses command line arguments form an options object
+    :param argv:
+    :return:
+    """
+    parser = ArgumentParser(prog="IRateYouRate")
+    parser.add_argument('--input_file_path', metavar='Input File Path', type=str)
+    parser.add_argument('--vectorization_tool', metavar='Vectorization Tool', type=str)
+    parser.add_argument('--doc2vec_training_count', metavar='Docvec Training Count', type=int)
+    parser.add_argument('--doc2vec_model_path', metavar='Docvec Model Path', type=str)
+    parser.add_argument('--ml_model_path', metavar='ML Model Path', type=str)
+
+    Options.options = parser.parse_args(argv, namespace=Options)
 
 
 if __name__ == "__main__":
