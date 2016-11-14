96bed1b7d21a830e98d734163097ef01381e72ea
Vineet John <vineet.john@uwaterloo.ca>
2016-11-14 15:45:42 -0500

    Removed Vectorization tool as an option

--- src/irateyourate/enums/vectorization_tools.py
+++
@@ -1,6 +0,0 @@
-# from enum import Enum
-#
-#
-# class VectorizationTools(Enum):
-#     gensim = 1
-#     fasttext = 2
--- src/irateyourate/irateyourate.py
+++ src/irateyourate/irateyourate.py
@@ -24,7 +24,6 @@ def parse_args(argv):
     """
     parser = ArgumentParser(prog="IRateYouRate")
     parser.add_argument('--input_file_path', metavar='Input File Path', type=str)
-    parser.add_argument('--vectorization_tool', metavar='Vectorization Tool', type=str)
     parser.add_argument('--doc2vec_training_count', metavar='Docvec Training Count', type=int)
     parser.add_argument('--doc2vec_model_path', metavar='Docvec Model Path', type=str)
     parser.add_argument('--ml_model_path', metavar='ML Model Path', type=str)
