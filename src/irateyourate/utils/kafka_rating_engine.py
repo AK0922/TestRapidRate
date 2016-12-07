aed37a041e740fccf405e0c1b7e872c614f725bc
Vineet John <vineet.john@uwaterloo.ca>
2016-12-07 18:50:47 -0500

    Coded validation and posted to OpenTSDB

--- src/irateyourate/utils/kafka_rating_engine.py
+++ src/irateyourate/utils/kafka_rating_engine.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-import sys
+import sys, requests, json, time, random
 
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
@@ -23,17 +23,58 @@ def tokenize_text(text):
     return all_words
 
 
+def post_to_opentsdb(payload):
+
+    url = "http://127.0.0.1:8070/api/put"
+    headers = {
+        'content-type': "application/json",
+    }
+    response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+
+    print("TSDB post Status Code: " + str(response.status_code))
+    print("Dumped batch to TSDB")
+
+
+def generate_tsdb_metric(metric, value, timestamp, tags):
+    if not tags:
+        tags = dict()
+        tags['source'] = "unknown1"
+
+    metric_dict = None
+    if metric and value and timestamp:
+        metric_dict = dict()
+        metric_dict['metric'] = metric
+        metric_dict['timestamp'] = timestamp
+        metric_dict['value'] = value
+        metric_dict['tags'] = tags
+
+    return metric_dict
+
+
 def infer_sentiment(rdd):
+
+    metrics = list()
+
     for (key, message) in rdd.collect():
 
         if message:
+
             tokenized_message = tokenize_text(str(message))
             message_vector = \
                 d2v_model.infer_vector(doc_words=tokenized_message)
             prediction = ml_model.predict([message_vector])
+
+            metric_dict = generate_tsdb_metric("sentiment", round(prediction[0], 2), int(time.time() * 1000), None)
             print(round(prediction[0], 2))
+
+            if metric_dict:
+                metrics.append(metric_dict)
+
         else:
             pass  # ignore if no message present
+
+    if len(metrics) > 0:
+        post_to_opentsdb(payload=metrics)
 
 
 if __name__ == "__main__":
